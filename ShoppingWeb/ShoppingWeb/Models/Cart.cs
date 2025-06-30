6a7bba9a3403a45233244f8180db31ea21e0072f
DESKTOP-MAC88L0\nguye <nguyenvu150203@gmail.com>
2025-06-30 22:00:29 +0700

    change model

--- ShoppingWeb/ShoppingWeb/Models/Cart.cs
+++ ShoppingWeb/ShoppingWeb/Models/Cart.cs
@@ -17,11 +17,23 @@ public partial class Cart
 
     public string ShippingAddress { get; set; } = null!;
 
+    public int? ProvinceId { get; set; }
+
+    public int? DistrictId { get; set; }
+
+    public int? WardId { get; set; }
+
     public DateTime? CreatedAt { get; set; }
 
     public DateTime? UpdatedAt { get; set; }
 
+    public virtual District? District { get; set; }
+
     public virtual ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
 
+    public virtual Province? Province { get; set; }
+
     public virtual User User { get; set; } = null!;
+
+    public virtual Ward? Ward { get; set; }
 }
