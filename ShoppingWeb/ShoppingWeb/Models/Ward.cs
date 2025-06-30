da3240a5b79fde71897088021de57e9e34089037
DESKTOP-MAC88L0\nguye <nguyenvu150203@gmail.com>
2025-06-30 22:39:08 +0700

    Reapply "add more models"
    This reverts commit d926e6320d6cb0612d0cce5589e634dbc07dea98.

---
+++ ShoppingWeb/ShoppingWeb/Models/Ward.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingWeb.Models;
+
+public partial class Ward
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int DistrictId { get; set; }
+
+    public virtual ICollection<Cart> Carts { get; set; } = new List<Cart>();
+
+    public virtual District District { get; set; } = null!;
+
+    public virtual ICollection<User> Users { get; set; } = new List<User>();
+}
