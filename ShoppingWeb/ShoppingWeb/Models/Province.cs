636cadb0ccb8ae14750344091a43a3973611b5d1
DESKTOP-MAC88L0\nguye <nguyenvu150203@gmail.com>
2025-06-30 22:18:12 +0700

    add more models

--- ShoppingWeb/ShoppingWeb/Models/Province.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ShoppingWeb.Models
-{
-    public class Province
-    {
-        public int ProvinceId { get; set; }
-        public string Name { get; set; } = null!;
-        public virtual ICollection<District> Districts { get; set; } = new List<District>();
-    }
-}
